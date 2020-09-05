4c926cb66503241b6db5fba026ee4924ec4b84d4
umeshpyati <Umesha.P@philips.com>
2020-09-05 12:33:16 +0530

    Updates

---
+++ precache-manifest.9091111e083d47357bc61f64d27aee2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f3d55a7bce7e53bfbc7a5420cfe54a",
+    "url": "/doList/index.html"
+  },
+  {
+    "revision": "7e7ef3c05e08346ff9e6",
+    "url": "/doList/static/css/main.0a0ddd88.chunk.css"
+  },
+  {
+    "revision": "b5c605a0efdd2725eb3c",
+    "url": "/doList/static/js/2.011b9e81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doList/static/js/2.011b9e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7ef3c05e08346ff9e6",
+    "url": "/doList/static/js/main.fe197aed.chunk.js"
+  },
+  {
+    "revision": "042ff2920decfad677ce",
+    "url": "/doList/static/js/runtime-main.dd18a1ea.js"
+  }
+]);
